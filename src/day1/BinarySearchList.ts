e9f1170f346ba92cdaf858ff31faf09848ecab4c
Vance McGrady <vancemcgrady@gmail.com>
2025-07-15 10:12:19 -0700

    crystal ball and binary search passed

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    while (lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+        // make sure to include "half-open" intervals [lo, hi) include lo, exclude hi
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    }
+    return false;
 }
+
+// bs_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7);
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // jump by sqrt of n
+    console.log("breaks: ");
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+    // breaking the first ball
+    // iterate through the array, incrementing i by sqrt of n each time until you find
+    // a truthy element (the ball breaks). In this scenario, the array is populated by
+    // trues and falses - a true means that the ball will break from that height (and all heights above.)
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) return i;
+    }
+    return -1;
 }
